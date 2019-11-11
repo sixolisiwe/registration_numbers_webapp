d0bd58c52345315bd101e258fe2a4f7adf4fbb46
sixolisiwe <sxlswmcb@gmail.com>
2019-11-11 16:09:19 +0200

    tests added

--- route.js
+++ route.js
@@ -6,26 +6,33 @@ module.exports = function names(townApp) {
         let town = await townApp.getNumber()
         res.render('index', {
             reg_numbers: town,
-            messages: req.flash('error'),
+            // messages: req.flash
         });
         
     }
 
     async function add(req, res) {
         let town = await townApp.getNumber()
+        
         let input = req.body.textboxTemp;
-        await townApp.setNumber(input);
-        if (input === '' ) {
-
-            req.flash('error', 'please enter a valid registration number')
-        }
-       else if(input !== town){
-            req.flash('error', 'please enter  a valid town tag')
-
+        let regex = /^[A-Za-z]{2}\s[0-9\s]{3}\s[0-9]{3}$/;
+        let regexTest = regex.test(input);
+       
+        if(regexTest === true){ 
+            await townApp.setNumber(input);
         }
         else{
-            req.flash('error', 'sucessfully added!')
+            req.flash('error', 'format not supported!')
+            
         }
+        if (input === '') {
+            req.flash('error', 'please enter a valid registration number')
+        } 
+        
+    //    else if(input.startsWith('CA') || input.startsWith('CY') ||input.startsWith('CJ') || input.startsWith('ca') || input.startsWith('cy') || input.startsWith('cj')=== false){
+            // req.flash('error', 'please enter  a valid town tag')
+        // }
+       
         
         res.redirect('/')
     }
@@ -34,13 +41,18 @@ module.exports = function names(townApp) {
         // console.log(req.body);
         
         let filterInput = req.body.radioBtnTemp;
-         console.log(await townApp.filterNumbers(filterInput), "3333333333333333");
-        
+        //  console.log(await townApp.filterNumbers(filterInput), "3333333333333333");
+        if(filterInput === '' || filterInput === undefined){
+            req.flash('error', 'please select a valid town')
+    
+        }
         res.render('index', {
         filtered : await townApp.filterNumbers(filterInput)
     
         // res.redirect('/')
+        
     })
+    
 }
 
     return {
--- test/regNumbers.test.js
+++ test/regNumbers.test.js
@@ -4,7 +4,8 @@ const pg = require("pg");
 const Pool = pg.Pool;
 
 // we are using a special test database for the tests
-const connectionString = process.env.DATABASE_URL || 'postgresql://postgres@localhost:5432/greet_test';
+const connectionString = process.env.DATABASE_URL || 'postgresql://codex:codex123@localhost/my_reg_numbers';
+
 
 const pool = new Pool({
     connectionString,
@@ -14,79 +15,92 @@ describe('The basic database web app', function () {
 
     beforeEach(async function () {
         // clean the tables before each test run
-        await pool.query("delete from namestoGreet;");
+        await pool.query("delete from reg_numbers;");
 
     });
 
     it('should pass the db test', async function () {
 
         // the Factory Function is called CategoryService
-        let GreetInstance = Greet(pool);
-        await GreetInstance.setName("sino")
+        let RegInstance = regNumb(pool);
+        await RegInstance.setNumber("ca 123 85")
 
-        let greet = await GreetInstance.getName();
-        assert.equal(1, greet.length);
-
-    });
-    it('should show the count of the names in the database', async function () {
-
-        
-        let GreetInstance = Greet(pool);
-        await GreetInstance.setName("sino")
-        await GreetInstance.setName("sbu")
-
-        let greet = await GreetInstance.getCount();
-        assert.equal(2, greet);
+        let number = await RegInstance.getNumber();
+        assert.equal(number.rows);
 
     });
 
-    it('should not count duplicate names in the database', async function () {
+    it('should show the print all the regnumbers in the database', async function () {
 
-      
-        let GreetInstance = Greet(pool);
-        await GreetInstance.setName("sino")
-        await GreetInstance.setName("sino")
 
-        let greet = await GreetInstance.getCount();
-        assert.equal(1, greet);
+        let RegInstance = regNumb(pool);
+        await RegInstance.setNumber("ca 123 85")
+        await RegInstance.setNumber("cy 123 25")
+        await RegInstance.setNumber("cj 823 85")
+        let number = await RegInstance.getNumber();
+        assert.equal(number.rows);
 
     });
-    it('should greet in the language selected', async function () {
 
-       
-        let GreetInstance = Greet(pool);
-    
-        await GreetInstance.langGreet("Sino", "English")
+    it('should return error messages when reg number format is invalid', async function () {
 
-        let greet = await GreetInstance.getGreeting();
-        assert.equal("Hello, Sino", greet);
+
+        let RegInstance = regNumb(pool);
+        await RegInstance.setNumber("ca123 85")
+
+
+        let number = await RegInstance.getNumber();
+        assert.equal(number.rows);
+
     });
 
- it('should greet in the language selected', async function () {
+    it('should return numbers for the town selected', async function () {
 
-   
-        let GreetInstance = Greet(pool);
-    
-        await GreetInstance.langGreet("Siwe", "Xhosa")
 
-        let greet = await GreetInstance.getGreeting();
-        assert.equal("Mholo, Siwe", greet);
+        let RegInstance = regNumb(pool);
+        await RegInstance.setNumber("ca 183 25")
+        // await RegInstance.setNumber("cj 623 85")
+
+        let number = await RegInstance.getNumber();
+        assert.equal(number.rows);
+        var town = 'CA'
+
+        let capetownRegs = await RegInstance.filterNumbers(town)
+        assert.equal(capetownRegs, "CA 183 25");
     });
-    it('should greet in the language selected', async function () {
 
-       
-        let GreetInstance = Greet(pool);
-    
-        await GreetInstance.langGreet("Lona", "Afrikaans")
 
-        let greet = await GreetInstance.getGreeting();
-        assert.equal("Hallo, Lona", greet);
+    it('should return numbers for the town selected', async function () {
+
+
+        let RegInstance = regNumb(pool);
+        await RegInstance.setNumber("cj 153 25")
+        // await RegInstance.setNumber("cj 623 85")
+
+        let number = await RegInstance.getNumber();
+        assert.equal(number.rows);
+        var town = 'CJ'
+
+        let capetownRegs = await RegInstance.filterNumbers(town)
+        assert.equal(capetownRegs, "CJ 153 25");
     });
-    
+
+    it('should return numbers for the town selected', async function () {
 
 
-    
-    
+        let RegInstance = regNumb(pool);
+        await RegInstance.setNumber("cy 113 25")
+        // await RegInstance.setNumber("cj 623 85")
+
+        let number = await RegInstance.getNumber();
+        assert.equal(number.rows);
+        var town = 'CY'
+
+        let capetownRegs = await RegInstance.filterNumbers(town)
+        assert.equal(capetownRegs, "CY 113 25");
+    });
+
+
 
     after(function () {
         pool.end();
