3e72c3410b0dfec7b5029f497a3af83b57e7f076
sixolisiwe <sxlswmcb@gmail.com>
2019-11-11 17:07:08 +0200

    database settings

--- test/regNumbers.test.js
+++ test/regNumbers.test.js
@@ -4,7 +4,7 @@ const pg = require("pg");
 const Pool = pg.Pool;
 
 // we are using a special test database for the tests
-const connectionString = process.env.DATABASE_URL || 'postgresql://codex:codex123@localhost/my_reg_numbers';
+const connectionString = process.env.DATABASE_URL || 'postgresql://postgres@localhost:5432/my_reg_numbers';
 
 
 const pool = new Pool({
