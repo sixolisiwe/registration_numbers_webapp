2c96d4d1598e1e4bea5a1df565be0d3f7b85d5c1
sixolisiwe <sxlswmcb@gmail.com>
2019-11-13 17:45:48 +0200

    error msg update

--- route.js
+++ route.js
@@ -18,25 +18,26 @@ module.exports = function names(townApp) {
         let regex = /^[A-Za-z]{2}\s[0-9\s]{3}\s[0-9]{3}$/;
         let regexTest = regex.test(input);
 
-        if (regexTest === true) {
+
+        // if (input !== undefined) {
+        //     req.flash('error', 'already entered!');
+        // }
+
+        if (input === '') {
+            req.flash('error', 'please enter a valid registration number');
+        } else if (regexTest === true) {
             await townApp.setNumber(input);
-        }
-        else {
+        } else {
             req.flash('error', 'format not supported');
 
         }
-        if (input === '') {
-            req.flash('error',    'please enter a valid registration number');
-        }
-
-
 
         res.redirect('/');
     }
 
     async function filtersApp(req, res) {
         let filterInput = req.body.radioBtnTemp;
-        
+
         if (filterInput === '' || filterInput === undefined) {
             req.flash('error', 'please select a valid town');
 
